cc118440d8f7b9c712894bff685b4e04109fe6f7
dev-abdullah-h <e.abdullah.haider.com>
2024-03-08 19:25:20 +0500

    remove junk files and do changes for the deployment test

--- backend/app.cjs
+++ backend/app.cjs
@@ -1,48 +1,45 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const uri = require('./pass.cjs')
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+require("dotenv").config();
 
-const app = express()
-app.use(bodyParser.json())
-app.use(cors())
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
-// 
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, "../dist")));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../dist', 'index.html')); // Send the main HTML file
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../dist", "index.html"));
 });
 
-// 
-app.get('/', (req, res) => {
-    res.send('Hello, this is Home')
-})
+app.get("/", (req, res) => {
+  res.send("Hello, this is Home");
+});
 
-mongoose.connect(uri).then(() => {
-    console.log('MongoDb Connected');
-    listen()
-})
-.catch((e) => {
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("MongoDb Connected");
+    listen();
+  })
+  .catch((e) => {
     console.log(e);
-}) 
-
-
-
+  });
 
 const listen = () => {
-    app.listen(3000, () => {
-        console.log('Server is listening on port 3000...');
-    })
-}
+  app.listen(3000, () => {
+    console.log("Server is listening on port 3000...");
+  });
+};
 
-const SignUpRoute = require('./routes/SignUp.cjs')
-app.use('/signUpApi', SignUpRoute)
+const SignUpRoute = require("./routes/SignUp.cjs");
+app.use("/signUpApi", SignUpRoute);
 
-const TodoRoute = require('./routes/Todo.cjs')
-app.use('/todoapi', TodoRoute)
+const TodoRoute = require("./routes/Todo.cjs");
+app.use("/todoapi", TodoRoute);
 
-const PostRoute = require('./routes/Post.cjs')
-app.use('/postapi', PostRoute)
+const PostRoute = require("./routes/Post.cjs");
+app.use("/postapi", PostRoute);
